3fde255f0796bccf0e760a9c98200567b307285a
gollumben <gollumben>
2016-07-30 18:04:49 +0200

    Course element ready for dynamic implementations.

--- service/static/service/js/views/course-element.js
+++ service/static/service/js/views/course-element.js
@@ -7,16 +7,16 @@ app.CourseElement = Backbone.View.extend({
 	<div class="container">
  
     		<h3>Tabs -left</h3>      		
-      		<div class="row">
- 				<div class="col-sm-3">
-			        <ul class="nav nav-pills white nav-stacked">
+      		<div class="row variwrapper">
+ 				<div class="col-sm-3 varibrowsing">
+			        <ul class="nav nav-pills nav-stacked">
 			          <li class="nav-item"><a href="#a"  class="nav-link active" data-toggle="tab">One</a></li>
 			          <li class="nav-item"><a href="#b"  class="nav-link" data-toggle="tab">Two</a></li>
 			          <li class="nav-item"><a href="#c"  class="nav-link" data-toggle="tab">Twee</a></li>
 			        </ul>
-			    </div>
-			    <div class="col-sm-3">
-			        <div class="tab-content">
+			    </div><vl>
+			    <div class="col-sm-9 varicontentholder">
+			        <div class="tab-content varicontent">
 				         <div class="tab-pane active" id="a">Lorem ipsum dolor sit amet, charetra varius quam sit amet vulputate. 
 				         Quisque mauris augue, molestie tincidunt condimentum vitae, gravida a libero.</div>
 				         <div class="tab-pane" id="b">Secondo sed ac orci quis tortor imperdiet venenatis. Duis elementum auctor accumsan. 
