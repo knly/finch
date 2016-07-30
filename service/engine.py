43c106100723ef8f79100f3008c2ecf89258e440
rooporoop <petermic@indiana.edu>
2016-07-30 12:44:45 +0200

    Fisher test
    Added Fisher test repository

--- service/engine.py
+++ service/engine.py
@@ -1,10 +1,30 @@
 from .models import *
+from .Fisher import *
 
 
 def chooseVariations(course, student):
     all_variations = course.variation_set.all()
 
-    # choose one
-    variation = all_variations[0]
+    # Perform Fisher test
 
+    # determine weights
+
+    
+    
+    # function that makes a weighted random selection
+    # http://stackoverflow.com/questions/3679694/a-weighted-version-of-random-choice
+    def weighted_choice(choices):
+        total = sum(w for c, w in choices)
+        r = random.uniform(0, total)
+        upto = 0
+        for c, w in choices:
+           if upto + w >= r:
+              return c
+           upto += w
+        assert False, "Shouldn't get here"
+    
+    # choose one
+    
+    
+    
     return Choice.objects.create(variation=variation, student=student)
