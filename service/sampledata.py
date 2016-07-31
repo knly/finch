316154a7a0560c8518c7989d063d8945940c6d31
NoaFeldman <feld.noa@gmail.com>
2016-07-31 14:38:39 +0200

    sample data for SE course, change grades to be 0 - 100 (instead of 0 - 1)

--- service/sampledata.py
+++ service/sampledata.py
@@ -4,11 +4,11 @@ import datetime
 import random
 import markdown
 
-c = Course.objects.create(title="Schrodinger Equation")
-content1 = markdown.markdown("Quantum Mechanics 5a - Schrödinger Equation 1 \n\n[![IMAGE ALT TEXT HERE](http://img.youtube.com/vi/aU_bd7fku90/0.jpg)](http://www.youtube.com/watch?v=aU_bd7fku90)", safe_mode=True)
-content2 = markdown.markdown("Schrodinger Equation - A simple derivation \n\n [![IMAGE ALT TEXT HERE](http://img.youtube.com/vi/IsX5iUKNT2k/0.jpg)](http://www.youtube.com/watch?v=IsX5iUKNT2k)", safe_mode=True)
+c = Course.objects.create(title="Quantum Mechanics", description="Understanding the foundations of modern physics.")
+content1 = markdown.markdown("# Lesson 1 \n\n Let's learn using a **simple derivation** of the equation. \n\n [![IMAGE ALT TEXT HERE](http://img.youtube.com/vi/IsX5iUKNT2k/0.jpg)](http://www.youtube.com/watch?v=IsX5iUKNT2k)", safe_mode=True)
+content2 = markdown.markdown("# Lesson 1 \n\n Let's learn through understanding the **concepts** of the equation. \n\n[![IMAGE ALT TEXT HERE](http://img.youtube.com/vi/aU_bd7fku90/0.jpg)](http://www.youtube.com/watch?v=aU_bd7fku90)", safe_mode=True)
 test_content = markdown.markdown("Why don't we experience the wave nature of matter on our everyday lives? \n\n \n\n a) The particles are connected in a complex system which suppresses the effect. \n\n b) It is not yet clear to science. \n\n c) The wavelength is to small for our scales.", safe_mode=True)
-t = Test.objects.create(course=c,content="Isn't Schrodinger Equation great?", correct_answer="c")
+t = Test.objects.create(course=c,content=test_content, correct_answer="c")
 v1 = Variation.objects.create(course=c,description="Derivation")
 l1 = Lesson.objects.create(variation=v1, index=0, content=content1)
 v2 = Variation.objects.create(course=c,description="Conceptual")
@@ -35,5 +35,5 @@ for i in range(20):
         finished_course = False
     r = Result.objects.create(test=t,
                           choice=ch,
-                          score=10+10*random.random(),
+                          score=30+random.randrange(30),
                           finished_course=finished_course)
